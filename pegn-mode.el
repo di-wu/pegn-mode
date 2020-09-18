7644d9ac992c6ff52441bc223fe909335f703be2
Quint Daenen <me@di-wu.be>
2020-09-18 10:49:32 +0200

    Add reserved ids and comments.

--- pegn-mode.el
+++ pegn-mode.el
@@ -29,8 +29,20 @@
         ; 1. M-x eval-expression
         ; 2. (regexp-opt '("LIST" "OF" "WORDS") t)
         ; 3. wrapped in < and > to indicate only surrounded by space or beginning/end-of-file.
-   '("\\<\\(SP\\)\\>" . font-lock-builtin-face)
-   '("\\('\\w*'\\)" . font-lock-variable-name-face))
+   ; These define all the reserved PEGN token constants:
+   ; "TAB" "CRLF" "CR" "LF" "SP" "VT" "FF" "NOT" "BANG" "DQ" "HASH" "DOLLAR" "PERCENT"
+   ; "AND" "SQ" "LPAREN" "RPAREN" "STAR" "PLUS" "COMMA" "DASH" "MINUS" "DOT" "SLASH"
+   ; "COLON" "SEMI" "LT" "EQ" "GT" "QUERY" "QUESTION" "AT" "LBRAKT" "BKSLASH" "RBRAKT"
+   ; "CARET" "UNDER" "BKTICK" "LCURLY" "LBRACE" "BAR" "PIPE" "RCURLY" "RBRACE" "TILDE"
+   ; "UNKNOWN" "REPLACE" "MAXRUNE" "MAXASCII" "MAXLATIN" "LARROW" "RARROW" "LLARROW"
+   ; "RLARROW" "LARROWF" "LFAT" "RARROWF" "RFAT" "WALRUS"
+   '("\\<\\(A\\(?:ND\\|T\\)\\|B\\(?:A\\(?:NG\\|R\\)\\|K\\(?:SLASH\\|TICK\\)\\)\\|C\\(?:ARET\\|O\\(?:LON\\|MMA\\)\\|R\\(?:LF\\)?\\)\\|D\\(?:ASH\\|O\\(?:LLAR\\|T\\)\\|Q\\)\\|EQ\\|FF\\|GT\\|HASH\\|L\\(?:ARROWF?\\|BRA\\(?:CE\\|KT\\)\\|CURLY\\|FAT\\|LARROW\\|PAREN\\|[FT]\\)\\|M\\(?:AX\\(?:ASCII\\|LATIN\\|RUNE\\)\\|INUS\\)\\|NOT\\|P\\(?:ERCENT\\|IPE\\|LUS\\)\\|QUE\\(?:RY\\|STION\\)\\|R\\(?:ARROWF?\\|BRA\\(?:CE\\|KT\\)\\|CURLY\\|EPLACE\\|FAT\\|LARROW\\|PAREN\\)\\|S\\(?:EMI\\|LASH\\|TAR\\|[PQ]\\)\\|T\\(?:AB\\|ILDE\\)\\|UN\\(?:DER\\|KNOWN\\)\\|VT\\|WALRUS\\)\\>" . font-lock-constant-face)
+   ; These define all the reserved PEGN check and class identifiers:
+   ; "alphanum" "alpha" "any" "bitdig" "control" "digit" "hexdig" "lowerhex" "lower"
+   ; "octdig" "punct" "quotable" "sign" "upperhex" "upper" "visible" "ws" "alnum" "ascii"
+   ; "blank" "cntrl" "graph" "print" "space" "word" "xdigit"
+   ; "EndLine"
+   '("\\<\\(EndLine\\|a\\(?:l\\(?:num\\|pha\\(?:num\\)?\\)\\|ny\\|scii\\)\\|b\\(?:itdig\\|lank\\)\\|c\\(?:\\(?:ntr\\|ontro\\)l\\)\\|digit\\|graph\\|hexdig\\|lower\\(?:hex\\)?\\|octdig\\|p\\(?:\\(?:rin\\|unc\\)t\\)\\|quotable\\|s\\(?:ign\\|pace\\)\\|upper\\(?:hex\\)?\\|visible\\|w\\(?:ord\\|s\\)\\|xdigit\\)\\>" . font-lock-constant-face))
   "Minimal highlighting expressions for PEGN mode.")
 
 (defvar pegn-font-lock-keywords pegn-font-lock-keywords-1
@@ -39,19 +51,20 @@
 (defun pegn-indent-line ()
   "Indent current line as PEGN code.")
 
-(defvar pegn-mode-syntax-table
-  (let ((pegn-mode-syntax-table (make-syntax-table)))
-    pegn-mode-syntax-table)
-   "Syntax table dor pegn-mode.")
+(defvar pegn-mode-syntax-table nil "Syntax table for pegn-mode.")
+(when (not pegn-mode-syntax-table)
+  (setq pegn-mode-syntax-table (make-syntax-table))
+  ;; Comment delimiters.
+  (modify-syntax-entry ?\# "<" pegn-mode-syntax-table)
+  (modify-syntax-entry ?\n ">" pegn-mode-syntax-table)
+  )
 
 (defun pegn-mode ()
   (interactive)
   (kill-all-local-variables)
   (use-local-map pegn-mode-map)
   (set-syntax-table pegn-mode-syntax-table)
-  ; font-lock
   (set (make-local-variable 'font-lock-defaults) '(pegn-font-lock-keywords))
-  ; indent
   (set (make-local-variable 'indent-line-function) 'pegn-indent-line)
   (setq major-mode 'pegn-mode)
   (setq mode-name "PEGN")
